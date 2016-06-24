706b76d13d364bb0988e53ad90fbf97768b5bfe1
pedro <pjcr35@hotmail.com>
2016-06-24 17:47:05 -0400

    Mi Blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,6 +26,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
+
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
